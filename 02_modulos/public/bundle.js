c21cac3e645c605820ea1df66534075a8d43b84c
Stefan <trfstephan@gmail.com>
2023-10-22 11:54:40 +0200

    rollup

---
+++ 02_modulos/public/bundle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var carrito = () => {
+    console.log('Producto agregado al carrito');
+};
+
+// Así importamos la función carrito del archivo carrito
+
+// Así llamamos la función carrito
+console.log('Archivo incial');
+carrito();
--- 02_modulos/src/carrito.js
+++ 02_modulos/src/carrito.js
@@ -1,3 +1,3 @@
 export default () => {
-    console.log('Producto siuuuu Agregado al carrito')
+    console.log('Producto agregado al carrito')
 }
