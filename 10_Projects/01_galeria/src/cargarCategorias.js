f74c601ce9a64834028ba7c193b4d344c318f949
Stefan <trfstephan@gmail.com>
2023-11-06 08:28:01 +0100

    node packages

--- 10_Projects/01_galeria/src/cargarCategorias.js
+++ 10_Projects/01_galeria/src/cargarCategorias.js
@@ -1,7 +1,7 @@
 // Archivo con la funcionalidad para cargar las categorias
 
 // Accedemos al archivo de las categorias
-// Importamos los datos como dataCategorias 
+// Importamos los datos como dataCategorias  
 import dataCategorias from './datos/categorias'
 // Desestructuramos, obtenemos las categorias y las guardamos dentro de una variable (categorias)
 const { categorias } = dataCategorias
